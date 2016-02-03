664934b7e0228c97b1f656ba5a9db95c393d7da0
mike-carter <mikecarter179@gmail.com>
2016-02-03 11:00:09 -0500

    Added some code to the Book.java constructor

--- model/Book.java
+++ model/Book.java
@@ -37,7 +37,42 @@ public class Book extends EntityBase
 		Vector<Properties> allDataRetrieved = getSelectQueryResult(query);
 		
 		// We must get at least one book
-		
+		if (allDataRetrieved != null) 
+		{
+			int size = allDataRetrieved.size();
+			
+			// There should be exactly one book. Any more will be an error.
+			if (size != 1)
+			{
+				throw new InvalidPrimaryKeyException(
+					String.format("Multiple Books matching id : %s found", bookId)
+					);
+			}
+			else 
+			{
+				// Copy all retrived data into persistant state.
+				Properties retrievedBookData = allDataRetrieved.elementAt(0);
+				persistantState = new Properties();
+				
+				Enumeration allKeys = retrievedBookData.propertyNames();
+				while (allKeys.hasMoreElements()) 
+				{
+					String nextKey = (String)allKeys.nextElement();
+					String nextValue = retrievedBookData.getProperty(nextKey);
+					
+					if (nextValue != null) 
+					{
+						persistentState.setProperty(nextKey, nextValue);
+					}
+				}
+			}
+		}
+		else // If no book was found at this Id, throw an exception
+		{
+			throw new InvalidPrimaryKeyException(
+				String.format("No Book matching id : %s found", bookId)
+				);
+		}
 	}
 	
 	/**
