63fd21e23ca0289e4220000f024dd3e79a2a2a44
Michael Carter <mikecarter179@gmail.com>
2016-02-09 10:32:28 -0500

    All model classes are compiling

--- model/PatronCollection.java
+++ model/PatronCollection.java
@@ -30,27 +30,196 @@ public class PatronCollection extends EntityBase
 		patrons = new Vector<Patron>();
 	}
 
-	public void findPatronsOlderThan(String year)
+	/** */
+	//-----------------------------------------------------------
+	public void findPatronsOlderThan(String year) throws InvalidPrimaryKeyException
 	{
+		String query = "SELECT * FROM " + myTableName + " WHERE (dateOfBirth < " + year + "";
+		Vector allDataRetrieved = getSelectQueryResult(query);
 
+		if (allDataRetrieved != null)
+		{
+			patrons = new Vector<Patron>();
+
+			for(int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextPatronData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Patron patron = new Patron(nextPatronData);
+
+				if(patron != null)
+				{
+					addPatron(patron);
+				}
+			}
+		}
+		else
+		{
+			throw new InvalidPrimaryKeyException("No patrons older than: " + year + " found");
+		}
 	}
 
-	public void findPatronsNewerThan(String year)
+	public void findPatronsNewerThan(String year) throws InvalidPrimaryKeyException
 	{
+		String query = "SELECT * FROM " + myTableName + " WHERE (dateOfBirth > " + year + "";
+		Vector allDataRetrieved = getSelectQueryResult(query);
 
+		if (allDataRetrieved != null)
+		{
+			patrons = new Vector<Patron>();
+
+			for(int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextPatronData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Patron patron = new Patron(nextPatronData);
+
+				if(patron != null)
+				{
+					addPatron(patron);
+				}
+			}
+		}
+		else
+		{
+			throw new InvalidPrimaryKeyException("No patrons newer than: " + year + " found");
+		}
 	}
 
-	public void findPatronsAtZipCode(String title)
+	public void findPatronsAtZipCode(String zip) throws InvalidPrimaryKeyException
 	{
+		String query = "SELECT * FROM " + myTableName + " WHERE (zip = " + zip + "";
+		Vector allDataRetrieved = getSelectQueryResult(query);
 
+		if (allDataRetrieved != null)
+		{
+			patrons = new Vector<Patron>();
+
+			for(int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextPatronData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Patron patron = new Patron(nextPatronData);
+
+				if(patron != null)
+				{
+					addPatron(patron);
+				}
+			}
+		}
+		else
+		{
+			throw new InvalidPrimaryKeyException("No patrons at zip code : " + zip + " found");
+		}
 	}
 
-	public void findPatronsWithNameLike(String author)
+	public void findPatronsWithNameLike(String name) throws InvalidPrimaryKeyException
 	{
+		String query = "SELECT * FROM " + myTableName + " WHERE name LIKE " + name + "";
+		Vector allDataRetrieved = getSelectQueryResult(query);
 
+		if (allDataRetrieved != null)
+		{
+			patrons = new Vector<Patron>();
+
+			for(int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextPatronData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Patron patron = new Patron(nextPatronData);
+
+				if(patron != null)
+				{
+					addPatron(patron);
+				}
+			}
+		}
+		else
+		{
+			throw new InvalidPrimaryKeyException("No patrons with similar name to : " + name + " found");
+		}
 	}
 
-	//-----------------------------------------------------------------------------------
+	private int findIndexToAdd(Patron p)
+	{
+		int low = 0;
+		int high = patrons.size()-1;
+		int middle;
+
+		while (low <= high)
+		{
+			middle = (low+high)/2;
+
+			Patron midSession = patrons.elementAt(middle);
+
+			int result = Patron.compare(p, midSession);
+
+			if (result == 0)
+			{
+				return middle;
+			}
+			else if (result < 0)
+			{
+				high = middle-1;
+			}
+			else
+			{
+				low = middle+1;
+			}
+		}
+		return low;
+	}
+
+	public Object getState(String key)
+	{
+		if (key.equals("Patrons"))
+			return patrons;
+		else if (key.equals("PatronList"))
+			return this;
+		return null;
+	}
+
+	public Patron retrieve(String patronId)
+	{
+		Patron retValue = null;
+		for (int cnt = 0; cnt < patrons.size(); cnt++)
+		{
+			Patron nextPatron = patrons.elementAt(cnt);
+			String nextPatronNum = (String)nextPatron.getState("patronId");
+			if (nextPatronNum.equals(patronId) == true)
+			{
+				retValue = nextPatron;
+				break;
+			}
+		}
+		return retValue;
+	}
+
+	/** */
+	//-----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+
+	/** */
+	//-----------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		myRegistry.updateSubscribers(key, this);
+	}
+	
+	/** */
+	//-----------------------------------------------------------
+	private void addPatron(Patron p)
+	{
+		//patrons.add(a);
+		int index = findIndexToAdd(p);
+		patrons.insertElementAt(p, index); // To build up a collection sorted on some key
+	}
+
+	/** */
+	//-----------------------------------------------------------
 	protected void initializeSchema(String tableName)
 	{
 		if (mySchema == null)
--- model/TransactionCollection.java
+++ model/TransactionCollection.java
@@ -30,12 +30,128 @@ public class TransactionCollection extends EntityBase
 		transactions = new Vector<Transaction>();
 	}
 
+	/** */
+	//-----------------------------------------------------------
 	public void findMatchingTransactions(String bookId, String patronId, String dateOfTrans)
+		throws InvalidPrimaryKeyException
 	{
+		String query = String.format("SELECT * FROM %s WHERE bookId = '%s' AND patronId = '%s' AND dateOfTrans = '%s'",
+									 myTableName,
+									 bookId,
+									 patronId,
+									 dateOfTrans);
+		Vector allDataRetrieved = getSelectQueryResult(query);
 
+		if (allDataRetrieved != null)
+		{
+			transactions = new Vector<Transaction>();
+
+			for(int cnt = 0; cnt < allDataRetrieved.size(); cnt++)
+			{
+				Properties nextTransData = (Properties)allDataRetrieved.elementAt(cnt);
+
+				Transaction trans = new Transaction(nextTransData);
+
+				if(trans != null)
+				{
+					addTransaction(trans);
+				}
+			}
+		}
+		else
+		{
+			throw new InvalidPrimaryKeyException(String.format("No transactions with : bookId='%s', patronId='%s', date='%s' found",
+															   bookId,
+															   patronId,
+															   dateOfTrans));
+		}
 	}
 
-	//-----------------------------------------------------------------------------------
+	/** */
+	//-----------------------------------------------------------
+	private int findIndexToAdd(Transaction t)
+	{
+		int low = 0;
+		int high = transactions.size()-1;
+		int middle;
+
+		while (low <= high)
+		{
+			middle = (low+high)/2;
+
+			Transaction midSession = transactions.elementAt(middle);
+
+			int result = Transaction.compare(t, midSession);
+
+			if (result == 0)
+			{
+				return middle;
+			}
+			else if (result < 0)
+			{
+				high = middle-1;
+			}
+			else
+			{
+				low = middle+1;
+			}
+		}
+		return low;
+	}
+
+	/** */
+	//-----------------------------------------------------------
+	public Object getState(String key)
+	{
+		if (key.equals("Transactions"))
+			return transactions;
+		else if (key.equals("TransactionList"))
+			return this;
+		return null;
+	}
+
+	/** */
+	public Transaction retrieve(String patronId)
+	{
+		Transaction retValue = null;
+		for (int cnt = 0; cnt < transactions.size(); cnt++)
+		{
+			Transaction nextTransaction = transactions.elementAt(cnt);
+			String nextTransactionNum = (String)nextTransaction.getState("patronId");
+			if (nextTransactionNum.equals(patronId) == true)
+			{
+				retValue = nextTransaction;
+				break;
+			}
+		}
+		return retValue;
+	}
+
+	/** */
+	//-----------------------------------------------------------
+	public void updateState(String key, Object value)
+	{
+		stateChangeRequest(key, value);
+	}
+
+	/** */
+	//-----------------------------------------------------------
+	public void stateChangeRequest(String key, Object value)
+	{
+		myRegistry.updateSubscribers(key, this);
+	}
+	
+	/** */
+	//-----------------------------------------------------------
+	private void addTransaction(Transaction t)
+	{
+		//transactions.add(a);
+		int index = findIndexToAdd(t);
+		transactions.insertElementAt(t, index); // To build up a collection sorted on some key
+	}
+
+	/** */
+	//-----------------------------------------------------------
 	protected void initializeSchema(String tableName)
 	{
 		if (mySchema == null)
