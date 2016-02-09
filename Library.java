e6527db4ed9b716c4f1a392c97dc159f5ca2c51d
Caleb Butcher <butcher.caleb@gmail.com>
2016-02-09 10:36:16 -0500

    DBConfig and Library Start

---
+++ Library.java
@@ -0,0 +1,48 @@
+// specify the package
+
+// system imports
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.io.FileOutputStream;
+import java.io.File;
+/* GUI IMPORTS
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+*/
+//project imports
+import event.Event;
+import event.EventLog;
+import common.PropertyFile;
+
+import model.*;
+import userinterface.MainStageContainer;
+import userinterface.WindowPosition;
+
+public class Library extends Application
+{
+  private Stage mainStage;
+
+  public void start(Stage primaryStage)
+  {
+    System.out.println("Library Version 1.00");
+    System.out.println("© 2016 Michael Carter and Caleb Butcher");
+
+    
+  }
+}
